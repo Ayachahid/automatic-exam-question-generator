68646ccfe0a410b7c44a91e38101c497a40a287c
Aya Chahid <ayaa.chahid.alaoui@gmail.com>
2026-03-14 22:24:12 +0100

    Remove .history backup files

--- .history/src/data/loaders/web_20260314201740.py
+++
@@ -1,8 +0,0 @@
-from .base import BaseLoader
-import requests
-from bs4 import BeautifulSoup #
-class WebLoader(BaseLoader):
-    def load(self, url):
-        r = requests.get(url)
-        soup = BeautifulSoup(r.text, "html.parser")
-        return soup.get_text()
